79feaf39af01582717f05d5fe73301488811217b
Andy Kuang <gnauk.ydna@hotmail.com>
2025-06-02 13:29:24 -0700

    Rename component and refactor tests

--- tests/simpletest.spec.js
+++ tests/simpletest.spec.js
@@ -3,7 +3,8 @@ import {test, expect, Page} from '@playwright/test';
 const {HomePage} = require('../pages/home.page');
 const {AnimePage} = require('../pages/anime.page');
 
-const {SearchBar} = require('../components/searchbar.component');
+const {CookieBanner} = require ('../components/cookiebanner.component');
+const {MenuBar} = require('../components/searchbar.component');
 
 test.beforeEach(async ({page}) => {
 	await page.goto('https://myanimelist.net/');
@@ -12,18 +13,24 @@ test.beforeEach(async ({page}) => {
 	await expect(page).toHaveTitle(/MyAnimeList\.net/);
 });
 
-test.describe('Clear banner and execute search', () => {
-	test('Go to home page and clear banner', async ({page}) => {
-		const homePage = new HomePage(page);
-		await expect(homePage.bannerLocator).toBeVisible();
-		await homePage.bannerOKButton.click();
-		await expect(homePage.bannerLocator).not.toBeVisible();
-	});
+test.describe('Cookie Banner tests', () => {
+	test('Clear cookie banner and confirm banner no longer appears', async ({page}) => {
+		const cookieBanner = new CookieBanner(page);
+		await expect(cookieBanner.bannerLocator).toBeVisible();
+		await cookieBanner.bannerOKButton.click();
+		await expect(cookieBanner.bannerOKButton).not.toBeVisible();
 
+		await page.reload();
+		await page.waitForTimeout(5000);
+		await expect(cookieBanner.bannerOKButton).not.toBeVisible();
+	});
+});
+
+test.describe('Execute a search on the menu bar', () => {
 	test ('Execute a search', async ({page}) => {
-		const searchBar = new SearchBar(page);
-		await searchBar.inputSearchText('Shingeki no Kyojin');
-		await searchBar.clickSearchResult('Shingeki no Kyojin Season 3 Part 2');
+		const menuBar = new MenuBar(page);
+		await menuBar.inputSearchText('Shingeki no Kyojin');
+		await menuBar.clickSearchResult('Shingeki no Kyojin Season 3 Part 2');
 
 		const animePage = new AnimePage(page);
 		await expect(animePage.body).toBeVisible();
